672bfc3d004f59f873ff68b5a018cc57e06f63b6
holland <514503337@qq.com>
2021-03-29 17:41:05 +0800

    相机开发

--- app/src/main/java/com/holland/myapp/js_interface/JsInterface.kt
+++ app/src/main/java/com/holland/myapp/js_interface/JsInterface.kt
@@ -1,10 +1,21 @@
 package com.holland.myapp.js_interface
 
 import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
 import android.webkit.JavascriptInterface
 import android.webkit.WebView
 import android.widget.Toast
+import androidx.core.app.ActivityCompat.startActivityForResult
+import androidx.core.content.FileProvider
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class JsInterface(private val activity: Activity, private val webView: WebView) {
@@ -12,5 +23,67 @@ class JsInterface(private val activity: Activity, private val webView: WebView) 
     fun showToast(msg: String?) {
         Log.d(this::class.java.name, "do function: showToast, info: $msg")
         Toast.makeText(activity, "安卓提示: $msg", Toast.LENGTH_SHORT).show()
+    }
+
+    @JavascriptInterface
+    fun camera() {
+        if (!activity.packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)) {
+            Toast.makeText(activity, "未打开相机权限", Toast.LENGTH_SHORT).show()
+        }
+
+//        createImageFile()
+        dispatchTakePictureIntent()
+//        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+//            takePictureIntent.resolveActivity(activity.packageManager)?.also {
+//                startActivityForResult(activity, takePictureIntent, 1, null)
+//            }
+//        }
+    }
+
+    lateinit var currentPhotoPath: String
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = activity.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
+    }
+
+    val REQUEST_TAKE_PHOTO = 1
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            takePictureIntent.resolveActivity(activity.packageManager)?.also {
+                // Create the File where the photo should go
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    null
+                }
+                // Continue only if the File was successfully created
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        activity,
+                        "com.holland.myapp",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(activity,takePictureIntent, REQUEST_TAKE_PHOTO,null)
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val REQUEST_IMAGE_CAPTURE = 1
     }
 }
